5fbb892c4e9ce5eddf1c91fca9db122700ca8606
Chris Morris <cmorris@ccistudios.com>
2015-08-13 11:20:40 -0400

    More robust easy post integration, including shipping labels

--- sites/all/modules/hb_label/js/order.js
+++ sites/all/modules/hb_label/js/order.js
@@ -1,26 +1,41 @@
 (function($){
     $(function(){
         $(".page-admin .entity-commerce-order").before("<form action='' method='post' id='order_product_scan' style='display:block; margin-bottom:1em;'><label>Scan products in this order: <input type='text' class='form-text' /></label></form>");
-        $(".page-admin form#order_product_scan").on("submit", formSubmit).find("input").focus();
+        $(".page-admin form#order_product_scan").on("submit", scanProductSubmit).find("input").focus();
+        $("#hb-label-shipping-label-form").on("submit", shippingLabelSubmit);
     });
     
-    function formSubmit()
+    function scanProductSubmit()
     {
         var pid = $(this).find("input").val();
         $(this).find(".message").remove();
-        if ($(".view-commerce-line-item-table [data-pid="+pid+"]").length)
+        if ($(".view-line-items-admin [data-pid="+pid+"]").length)
         {
-            $(".view-commerce-line-item-table [data-pid="+pid+"]").html("<img src='/misc/message-16-ok.png' title='Scanned' />")
-            .parents(".views-row").css("background-color","#C9EFC9");
-            $("body").append("<audio autoplay src='/sites/all/modules/hb_label/media/accept.mp3' style='position:absolute; left:-999999px;' />");
+            $(".view-line-items-admin [data-pid="+pid+"]").html("<img src='/misc/message-16-ok.png' title='Scanned' />")
+            .parents(".views-row").addClass("scanned").css("background-color","#C9EFC9");
+            playSound("accept");
+            
+            if ($(".view-line-items-admin .views-row").length == $(".view-line-items-admin .views-row.scanned").length)
+            {
+                $("#hb-label-shipping-label-form").removeClass("element-invisible");
+            }
         }
         else
         {
-            $("body").append("<audio autoplay src='/sites/all/modules/hb_label/media/reject.mp3' style='position:absolute; left:-999999px;' />");
+            playSound("reject");
             $(this).append("<div class='message'>Wrong/unknown product</div>");
         }
         
         $(this).find("input").val("").focus();
         return false;
+    }
+    function playSound(soundFile)
+    {
+        $("body").append("<audio autoplay src='/sites/all/modules/hb_label/media/"+soundFile+".mp3' style='position:absolute; left:-999999px;' />");
+    }
+    
+    function shippingLabelSubmit()
+    {
+        return confirm("This will generate and pay for a new shipping label.\nAn email will be sent to the customer with a tracking number.\nAre you sure?");
     }
 })(jQuery);
